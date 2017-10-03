f350f46031dcec5398af3d27a38d76280425fc5b
Greg <greg@Gregs-MacBook-Pro.local>
2017-10-03 09:31:02 -0400

    Updated to use Font-Awesome Chevrons for SimpleSignups

--- src/TimeView.js
+++ src/TimeView.js
@@ -6,108 +6,111 @@ var React = require('react'),
 	onClickOutside = require('react-onclickoutside').default
 	;
 
-var DateTimePickerTime = onClickOutside( createClass({
-	getInitialState: function() {
-		return this.calculateState( this.props );
+var DateTimePickerTime = onClickOutside(createClass({
+	getInitialState: function () {
+		return this.calculateState(this.props);
 	},
 
-	calculateState: function( props ) {
+	calculateState: function (props) {
 		var date = props.selectedDate || props.viewDate,
 			format = props.timeFormat,
 			counters = []
 			;
 
-		if ( format.toLowerCase().indexOf('h') !== -1 ) {
+		if (format.toLowerCase().indexOf('h') !== -1) {
 			counters.push('hours');
-			if ( format.indexOf('m') !== -1 ) {
+			if (format.indexOf('m') !== -1) {
 				counters.push('minutes');
-				if ( format.indexOf('s') !== -1 ) {
+				if (format.indexOf('s') !== -1) {
 					counters.push('seconds');
 				}
 			}
 		}
 
 		var daypart = false;
-		if ( this.state !== null && this.props.timeFormat.toLowerCase().indexOf( ' a' ) !== -1 ) {
-			if ( this.props.timeFormat.indexOf( ' A' ) !== -1 ) {
-				daypart = ( this.state.hours >= 12 ) ? 'PM' : 'AM';
+		if (this.state !== null && this.props.timeFormat.toLowerCase().indexOf(' a') !== -1) {
+			if (this.props.timeFormat.indexOf(' A') !== -1) {
+				daypart = (this.state.hours >= 12) ? 'PM' : 'AM';
 			} else {
-				daypart = ( this.state.hours >= 12 ) ? 'pm' : 'am';
+				daypart = (this.state.hours >= 12) ? 'pm' : 'am';
 			}
 		}
 
 		return {
-			hours: date.format( 'H' ),
-			minutes: date.format( 'mm' ),
-			seconds: date.format( 'ss' ),
-			milliseconds: date.format( 'SSS' ),
+			hours: date.format('H'),
+			minutes: date.format('mm'),
+			seconds: date.format('ss'),
+			milliseconds: date.format('SSS'),
 			daypart: daypart,
 			counters: counters
 		};
 	},
 
-	renderCounter: function( type ) {
-		if ( type !== 'daypart' ) {
-			var value = this.state[ type ];
-			if ( type === 'hours' && this.props.timeFormat.toLowerCase().indexOf( ' a' ) !== -1 ) {
-				value = ( value - 1 ) % 12 + 1;
+	renderCounter: function (type) {
+		if (type !== 'daypart') {
+			var value = this.state[type];
+			if (type === 'hours' && this.props.timeFormat.toLowerCase().indexOf(' a') !== -1) {
+				value = (value - 1) % 12 + 1;
 
-				if ( value === 0 ) {
+				if (value === 0) {
 					value = 12;
 				}
 			}
 			return React.createElement('div', { key: type, className: 'rdtCounter' }, [
-				React.createElement('span', { key: 'up', className: 'rdtBtn', onMouseDown: this.onStartClicking( 'increase', type ) }, '▲' ),
-				React.createElement('div', { key: 'c', className: 'rdtCount' }, value ),
-				React.createElement('span', { key: 'do', className: 'rdtBtn', onMouseDown: this.onStartClicking( 'decrease', type ) }, '▼' )
+				React.createElement('span', { key: 'up', className: 'rdtBtn', onMouseDown: this.onStartClicking('increase', type) }, [
+					React.createElement('i', { key: 'i', className: 'fa fa-chevron-up' })
+				]),
+				React.createElement('div', { key: 'c', className: 'rdtCount' }, value),
+				React.createElement('span', { key: 'do', className: 'rdtBtn', onMouseDown: this.onStartClicking('decrease', type) }, [
+					React.createElement('i', { key: 'i', className: 'fa fa-chevron-down' })
+				])
 			]);
 		}
 		return '';
 	},
 
-	renderDayPart: function() {
+	renderDayPart: function () {
 		return React.createElement('div', { key: 'dayPart', className: 'rdtCounter' }, [
-			React.createElement('span', { key: 'up', className: 'rdtBtn', onMouseDown: this.onStartClicking( 'toggleDayPart', 'hours') }, '▲' ),
-			React.createElement('div', { key: this.state.daypart, className: 'rdtCount' }, this.state.daypart ),
-			React.createElement('span', { key: 'do', className: 'rdtBtn', onMouseDown: this.onStartClicking( 'toggleDayPart', 'hours') }, '▼' )
+			React.createElement('span', { key: 'up', className: 'rdtBtn', onMouseDown: this.onStartClicking('toggleDayPart', 'hours') }, ''),
+			React.createElement('div', { key: this.state.daypart, className: 'rdtCount' }, this.state.daypart),
+			React.createElement('span', { key: 'do', className: 'rdtBtn', onMouseDown: this.onStartClicking('toggleDayPart', 'hours') }, '')
 		]);
 	},
 
-	render: function() {
+	render: function () {
 		var me = this,
-			counters = []
-		;
+			counters = [];
 
-		this.state.counters.forEach( function( c ) {
-			if ( counters.length )
-				counters.push( React.createElement('div', { key: 'sep' + counters.length, className: 'rdtCounterSeparator' }, ':' ) );
-			counters.push( me.renderCounter( c ) );
+		this.state.counters.forEach(function (c) {
+			if (counters.length)
+				counters.push(React.createElement('div', { key: 'sep' + counters.length, className: 'rdtCounterSeparator' }, ':'));
+			counters.push(me.renderCounter(c));
 		});
 
-		if ( this.state.daypart !== false ) {
-			counters.push( me.renderDayPart() );
+		if (this.state.daypart !== false) {
+			counters.push(me.renderDayPart());
 		}
 
-		if ( this.state.counters.length === 3 && this.props.timeFormat.indexOf( 'S' ) !== -1 ) {
-			counters.push( React.createElement('div', { className: 'rdtCounterSeparator', key: 'sep5' }, ':' ) );
+		if (this.state.counters.length === 3 && this.props.timeFormat.indexOf('S') !== -1) {
+			counters.push(React.createElement('div', { className: 'rdtCounterSeparator', key: 'sep5' }, ':'));
 			counters.push(
 				React.createElement('div', { className: 'rdtCounter rdtMilli', key: 'm' },
-					React.createElement('input', { value: this.state.milliseconds, type: 'text', onChange: this.updateMilli } )
-					)
-				);
+					React.createElement('input', { value: this.state.milliseconds, type: 'text', onChange: this.updateMilli })
+				)
+			);
 		}
 
 		return React.createElement('div', { className: 'rdtTime' },
 			React.createElement('table', {}, [
 				this.renderHeader(),
-				React.createElement('tbody', { key: 'b'}, React.createElement('tr', {}, React.createElement('td', {},
-					React.createElement('div', { className: 'rdtCounters' }, counters )
+				React.createElement('tbody', { key: 'b' }, React.createElement('tr', {}, React.createElement('td', {},
+					React.createElement('div', { className: 'rdtCounters' }, counters)
 				)))
 			])
 		);
 	},
 
-	componentWillMount: function() {
+	componentWillMount: function () {
 		var me = this;
 		me.timeConstraints = {
 			hours: {
@@ -131,57 +134,57 @@ var DateTimePickerTime = onClickOutside( createClass({
 				step: 1
 			}
 		};
-		['hours', 'minutes', 'seconds', 'milliseconds'].forEach( function( type ) {
-			assign(me.timeConstraints[ type ], me.props.timeConstraints[ type ]);
+		['hours', 'minutes', 'seconds', 'milliseconds'].forEach(function (type) {
+			assign(me.timeConstraints[type], me.props.timeConstraints[type]);
 		});
-		this.setState( this.calculateState( this.props ) );
+		this.setState(this.calculateState(this.props));
 	},
 
-	componentWillReceiveProps: function( nextProps ) {
-		this.setState( this.calculateState( nextProps ) );
+	componentWillReceiveProps: function (nextProps) {
+		this.setState(this.calculateState(nextProps));
 	},
 
-	updateMilli: function( e ) {
-		var milli = parseInt( e.target.value, 10 );
-		if ( milli === e.target.value && milli >= 0 && milli < 1000 ) {
-			this.props.setTime( 'milliseconds', milli );
-			this.setState( { milliseconds: milli } );
+	updateMilli: function (e) {
+		var milli = parseInt(e.target.value, 10);
+		if (milli === e.target.value && milli >= 0 && milli < 1000) {
+			this.props.setTime('milliseconds', milli);
+			this.setState({ milliseconds: milli });
 		}
 	},
 
-	renderHeader: function() {
-		if ( !this.props.dateFormat )
+	renderHeader: function () {
+		if (!this.props.dateFormat)
 			return null;
 
 		var date = this.props.selectedDate || this.props.viewDate;
 		return React.createElement('thead', { key: 'h' }, React.createElement('tr', {},
-			React.createElement('th', { className: 'rdtSwitch', colSpan: 4, onClick: this.props.showView( 'days' ) }, date.format( this.props.dateFormat ) )
+			React.createElement('th', { className: 'rdtSwitch', colSpan: 4, onClick: this.props.showView('days') }, date.format(this.props.dateFormat))
 		));
 	},
 
-	onStartClicking: function( action, type ) {
+	onStartClicking: function (action, type) {
 		var me = this;
 
-		return function() {
+		return function () {
 			var update = {};
-			update[ type ] = me[ action ]( type );
-			me.setState( update );
+			update[type] = me[action](type);
+			me.setState(update);
 
-			me.timer = setTimeout( function() {
-				me.increaseTimer = setInterval( function() {
-					update[ type ] = me[ action ]( type );
-					me.setState( update );
+			me.timer = setTimeout(function () {
+				me.increaseTimer = setInterval(function () {
+					update[type] = me[action](type);
+					me.setState(update);
 				}, 70);
 			}, 500);
 
-			me.mouseUpListener = function() {
-				clearTimeout( me.timer );
-				clearInterval( me.increaseTimer );
-				me.props.setTime( type, me.state[ type ] );
-				document.body.removeEventListener( 'mouseup', me.mouseUpListener );
+			me.mouseUpListener = function () {
+				clearTimeout(me.timer);
+				clearInterval(me.increaseTimer);
+				me.props.setTime(type, me.state[type]);
+				document.body.removeEventListener('mouseup', me.mouseUpListener);
 			};
 
-			document.body.addEventListener( 'mouseup', me.mouseUpListener );
+			document.body.addEventListener('mouseup', me.mouseUpListener);
 		};
 	},
 
@@ -192,35 +195,35 @@ var DateTimePickerTime = onClickOutside( createClass({
 		milliseconds: 3
 	},
 
-	toggleDayPart: function( type ) { // type is always 'hours'
-		var value = parseInt( this.state[ type ], 10) + 12;
-		if ( value > this.timeConstraints[ type ].max )
-			value = this.timeConstraints[ type ].min + ( value - ( this.timeConstraints[ type ].max + 1 ) );
-		return this.pad( type, value );
+	toggleDayPart: function (type) { // type is always 'hours'
+		var value = parseInt(this.state[type], 10) + 12;
+		if (value > this.timeConstraints[type].max)
+			value = this.timeConstraints[type].min + (value - (this.timeConstraints[type].max + 1));
+		return this.pad(type, value);
 	},
 
-	increase: function( type ) {
-		var value = parseInt( this.state[ type ], 10) + this.timeConstraints[ type ].step;
-		if ( value > this.timeConstraints[ type ].max )
-			value = this.timeConstraints[ type ].min + ( value - ( this.timeConstraints[ type ].max + 1 ) );
-		return this.pad( type, value );
+	increase: function (type) {
+		var value = parseInt(this.state[type], 10) + this.timeConstraints[type].step;
+		if (value > this.timeConstraints[type].max)
+			value = this.timeConstraints[type].min + (value - (this.timeConstraints[type].max + 1));
+		return this.pad(type, value);
 	},
 
-	decrease: function( type ) {
-		var value = parseInt( this.state[ type ], 10) - this.timeConstraints[ type ].step;
-		if ( value < this.timeConstraints[ type ].min )
-			value = this.timeConstraints[ type ].max + 1 - ( this.timeConstraints[ type ].min - value );
-		return this.pad( type, value );
+	decrease: function (type) {
+		var value = parseInt(this.state[type], 10) - this.timeConstraints[type].step;
+		if (value < this.timeConstraints[type].min)
+			value = this.timeConstraints[type].max + 1 - (this.timeConstraints[type].min - value);
+		return this.pad(type, value);
 	},
 
-	pad: function( type, value ) {
+	pad: function (type, value) {
 		var str = value + '';
-		while ( str.length < this.padValues[ type ] )
+		while (str.length < this.padValues[type])
 			str = '0' + str;
 		return str;
 	},
 
-	handleClickOutside: function() {
+	handleClickOutside: function () {
 		this.props.handleClickOutside();
 	}
 }));
